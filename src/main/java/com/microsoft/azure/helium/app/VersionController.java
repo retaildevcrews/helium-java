fb69dd89457743f5f7afd1ba205e5e309b4ccc60
Jyotsna Ravikumar <jyraviku@microsoft.com>
2020-01-23 15:14:13 -0600

    code review comments

--- src/main/java/com/microsoft/azure/helium/app/VersionController.java
+++ src/main/java/com/microsoft/azure/helium/app/VersionController.java
@@ -1,17 +1,12 @@
 package com.microsoft.azure.helium.app;
 
-import com.microsoft.azure.helium.Application;
 import com.microsoft.azure.helium.config.BuildConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Enumeration;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 
 @RestController
@@ -20,6 +15,7 @@ public class VersionController {
     BuildConfig buildConfig;
 
     @GetMapping("/version")
+    @Cacheable(value = "version")
     public String version() throws IOException {
         return buildConfig.getBuildVersion();
     }
--- src/main/java/com/microsoft/azure/helium/config/BuildConfig.java
+++ src/main/java/com/microsoft/azure/helium/config/BuildConfig.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
@@ -17,27 +17,15 @@ public class BuildConfig {
     BuildProperties buildProperties;
 
     public String getBuildVersion(){
-
+        //An Instant represents a moment on the timeline in UTC with a resolution of up to nanoseconds.
         Instant buildTime = buildProperties.getTime();
-
-        DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                .withZone(ZoneId.systemDefault());
-
-        System.out.println(DATE_TIME_FORMATTER.format(buildTime));
-
-        //major.minor.MMdd.hhmm
+        //major.minor.MMd.hhmm
         String major = buildProperties.getVersion();
-        DateTimeFormatter DATE_TIME_FORMATTER1 = DateTimeFormatter.ofPattern("MMDD")
-                .withZone(ZoneId.systemDefault());
+        DateTimeFormatter DATE_TIME_FORMATTER1 = DateTimeFormatter.ofPattern("MMDD").withZone( ZoneId.of("UTC"));
         String MMDD = DATE_TIME_FORMATTER1.format(buildTime);
-
-        DateTimeFormatter DATE_TIME_FORMATTER2 = DateTimeFormatter.ofPattern("hhmm")
-                .withZone(ZoneId.systemDefault());
+        DateTimeFormatter DATE_TIME_FORMATTER2 = DateTimeFormatter.ofPattern("hhmm").withZone( ZoneId.of("UTC"));
         String hhmm = DATE_TIME_FORMATTER2.format(buildTime);
-
-        String versionNo = major + "." + MMDD + "." + hhmm;
-        System.out.println(versionNo);
-        return versionNo ;
+        return  major + "." + MMDD + "." + hhmm;
 
     }
 }
