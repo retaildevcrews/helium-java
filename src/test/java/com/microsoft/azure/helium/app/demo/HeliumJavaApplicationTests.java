d11ee84497a0b6bf763a97c54e4162635b9b6f5e
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-02-14 20:10:09 +0000

    Adding base scaffolding

---
+++ src/test/java/com/microsoft/azure/helium/app/demo/HeliumJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.azure.helium.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliumJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
