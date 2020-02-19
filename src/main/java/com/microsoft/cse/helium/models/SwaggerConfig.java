5d44f10f211106d05851524c1ab723b56836c176
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-02-19 23:29:27 +0000

    Adding swagger libraries and somewhat working

---
+++ src/main/java/com/microsoft/cse/helium/models/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.microsoft.cse.helium.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfig {
+    @Autowired
+    private BuildConfig buildConfig;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Helium(Java)")
+                .description("Java app for bootstrapping your next Web Apps for Containers service using Key Vault and Managed Identities")
+                .version(buildConfig.getBuildVersion())
+                .build();
+    }
+}
