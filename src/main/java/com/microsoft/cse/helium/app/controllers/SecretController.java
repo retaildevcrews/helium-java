c57cd7e86e9bbe8074fff2f7376c76a6252cd351
Joseph Fultz <jofultz@microsoft.com>
2020-02-19 23:10:49 +0000

    local fn kv test

--- src/main/java/com/microsoft/cse/helium/app/controllers/SecretController.java
+++ src/main/java/com/microsoft/cse/helium/app/controllers/SecretController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
 
+import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.credentials.MSICredentials;
+import com.microsoft.azure.keyvault.KeyVaultClient;
+import com.microsoft.azure.keyvault.models.SecretBundle;
+
 @RestController
 @RequestMapping (value = "api/secret")
 class SecretController{
@@ -21,7 +26,13 @@ class SecretController{
 
     @GetMapping(value={"/",""}, produces = MediaType.TEXT_PLAIN_VALUE)
     public Mono<ResponseEntity<String>> getSecret(){
+
         String returnValue = "Return from getSecret()";
+
+        MSICredentials credentials = new MSICredentials(AzureEnvironment.AZURE);
+        KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
+        SecretBundle secret = keyVaultClient.getSecret("https://jf2he.vault.azure.net","CosmosKey","9228944c9038476f8f7d1be86c333c20");
+
 
         return Mono.just(ResponseEntity.ok()
             .body(returnValue));
