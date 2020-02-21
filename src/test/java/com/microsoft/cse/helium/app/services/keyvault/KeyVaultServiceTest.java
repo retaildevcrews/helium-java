b3938edc3267e1ec3cbe0e7e20cb9731f24668b5
Joseph Fultz <jofultz@microsoft.com>
2020-02-21 21:21:05 +0000

    KeyVaultService constructor + tests

--- src/test/java/com/microsoft/cse/helium/app/services/keyvault/KeyVaultServiceTest.java
+++ src/test/java/com/microsoft/cse/helium/app/services/keyvault/KeyVaultServiceTest.java
@@ -1,27 +1,16 @@
-import com.microsoft.cse.helium.app.services.keyvault.KeyVaultService;
-
-import static org.junit.Assert.assertEquals;
+package com.microsoft.cse.helium.app.services.keyvault;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-
-// import org.junit.runner.RunWith;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.mockito.junit.MockitoJUnitRunner;
-// import org.springframework.test.context.junit4.SpringRunner;
-// import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-//@RunWith(SpringRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class KeyVaultServiceTest {
 
     KeyVaultService keyVaultServiceTest;
@@ -36,19 +25,83 @@ public class KeyVaultServiceTest {
       // Do your cleanup here like close URL connection , releasing resources etc
     }
   
-    @Test(expected = IllegalArgumentException.class)
-    public void TestBadEnvironmentFlag() {        
-      keyVaultServiceTest = new KeyVaultService("1two-3", "BadValue");
+    //Constructor:EnvironmentFlag tests
+    @Test(expected = Exception.class)
+    public void TestBadEnvironmentFlag() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("One2-3", "BadValue");
     }
-  
+
+    @Test(expected = Exception.class)
+    public void TestEmptyEnvironmentFlag() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("One2-3", "");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestNullEnvironmentFlag() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("One2-3", null);
+    }
+
     @Test
-    public void TestGoodEnvironmentFlag() {
+    public void TestGoodEnvironmentFlag() throws Exception {
         keyVaultServiceTest = null;
-        keyVaultServiceTest = new KeyVaultService("1two-3", "CLI");
+        keyVaultServiceTest = new KeyVaultService("One2-thR33", "CLI");
         assertNotNull(keyVaultServiceTest);
 
         keyVaultServiceTest = null;
-        keyVaultServiceTest = new KeyVaultService("1two-3", "MSI");
+        keyVaultServiceTest = new KeyVaultService("One2-thR33", "MSI");
+        assertNotNull(keyVaultServiceTest);
+    }
+    // END Constructor:EnvironmentFlag tests
+
+    //Constructor:KeyVaultName tests
+    @Test(expected = Exception.class)
+    public void TestShortKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("kv", "MSI");
+    }
+  
+    @Test(expected = Exception.class)
+    public void TestLongKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("one234five6789ten1112thirteen", "MSI");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestEmptyKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("", "MSI");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestNullKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService(null, "MSI");
+    }
+    
+    @Test(expected = Exception.class)
+    public void TestTrailingHyphenKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("one2-3-", "MSI");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestConsecutiveHyphenKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("one2--3", "MSI");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestLeadingNumberKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("1one2--3", "MSI");
+    }
+
+    @Test(expected = Exception.class)
+    public void TestLeadingHyphenKeyVaultName() throws Exception {        
+      keyVaultServiceTest = new KeyVaultService("1one2--3", "MSI");
+    }
+
+    @Test
+    public void TestGoodKeyVaultName() throws Exception {
+        keyVaultServiceTest = null;
+        keyVaultServiceTest = new KeyVaultService("One2-thR33", "CLI");
+        assertNotNull(keyVaultServiceTest);
+
+        keyVaultServiceTest = null;
+        keyVaultServiceTest = new KeyVaultService("One2-thR33", "MSI");
         assertNotNull(keyVaultServiceTest);
     }
   } 
