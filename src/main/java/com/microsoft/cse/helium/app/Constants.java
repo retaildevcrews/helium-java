9132fa85a62ea9c7c0dc65a4a2e799413b2eeec2
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-03-03 18:18:44 -0800

    Actors Controller + CosmosDB access layer with async (#94)
    * findByActorId works
    
    * checkpoint checkin
    
    * both get single actor and get all actors works (no paging though)
    
    * Queries work without pagination
    
    * updating the repo with limiting code that works
    
    * small cleanup
    
    * all the paramters work
    
    * checkpoint checkin
    
    * refactoring
    
    * adding the application-dev.properties so we can keep our local changes separate from teh repo
    
    * documentation update in application.properties
    
    * removing unnecessary files and imports
    
    * removing unused imports and fixing Flux to Mono in Version
    
    * putting back the flag that I accidentally removed
    
    * undoing another change to the app props
    
    * removing tabs and replacing with spaces to get rid of formatting issues
    
    * moving the namespace for constatns in imports
    
    * splitting out the custom query into a dao class
    
    * removing jcenter, an unnecessary dependency
    
    * updating the dao with final strings with formatting to insert params

---
+++ src/main/java/com/microsoft/cse/helium/app/Constants.java
@@ -0,0 +1,17 @@
+package com.microsoft.cse.helium.app;
+
+//
+
+/**
+ * Constants
+ */
+public final class Constants {
+
+    public static final String DEFAULT_GENRE_COLLECTION_NAME = "genres";
+    public static final String DEFAULT_ACTOR_COLLECTION_NAME = "actors";
+    public static final String DEFAULT_MOVIE_COLLECTION_NAME = "movies";
+    public static final String DEFAULT_FEATURED_COLLECTION_NAME = "featured";
+    public static final Integer DefaultPageSize = 100;
+    public static final Integer MaxPageSize = 1000;
+    public static final String webInstanceRole = "WEBSITE_ROLE_INSTANCE_ID";
+}
--- src/main/java/com/microsoft/cse/helium/app/controllers/VersionController.java
+++ src/main/java/com/microsoft/cse/helium/app/controllers/VersionController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import com.microsoft.cse.helium.app.config.BuildConfig;;
 
 @RestController
@@ -20,8 +22,8 @@ public class VersionController {
       value="/version",
       produces = MediaType.TEXT_PLAIN_VALUE)
       
-  public Flux<String> version(ServerHttpResponse response) {
+  public Mono<String> version(ServerHttpResponse response) {
     response.setStatusCode(HttpStatus.OK);
-    return Flux.just(buildConfig.getBuildVersion());
+    return Mono.just(buildConfig.getBuildVersion());
   }
 }
