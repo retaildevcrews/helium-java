94f7762e006ef3d0ac05d67bb3db5fb3e41c936e
Joseph Fultz <jofultz@microsoft.com>
2020-03-04 16:49:34 +0000

    removed commened code

--- src/main/java/com/microsoft/cse/helium/app/services/configuration/ConfigurationService.java
+++ src/main/java/com/microsoft/cse/helium/app/services/configuration/ConfigurationService.java
@@ -12,8 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 import java.io.IOException;
-// import com.microsoft.azure.spring.data.cosmosdb.config.AbstractCosmosConfiguration;
-// import com.microsoft.azure.spring.data.cosmosdb.config.CosmosDBConfig;
+
 import org.springframework.context.annotation.Bean;
 
 import com.microsoft.azure.keyvault.models.CertificateBundle;
@@ -27,33 +26,16 @@ public class ConfigurationService implements IConfigurationService
 {
     private static final Logger _logger = LoggerFactory.getLogger(ConfigurationService.class);
 
-    //@Autowired
     private IKeyVaultService _keyVaultService;
 
     @Autowired
     public ConfigurationService(IKeyVaultService kvService){
         _keyVaultService = kvService;
-        //retrieve keys and store in dictionary
-        //String secretValue = _keyVaultService.getSecret("CosmosDbUrl").block();
-        //List<String> = _keyVaultService.listSecrets().block();
         Dictionary<String, String> secrets = _keyVaultService.getSecrets();
         configEntries = secrets;
     }
 
     @Getter
     Dictionary configEntries = new Hashtable<String, String>();
-
-    
-    //@Bean
-    /*
-    public CosmosDBConfig getConfig() {
-        RequestOptions options = getRequestOptions();
-        ConnectionPolicy policy = new ConnectionPolicy();
-        RetryOptions retryOptions = new RetryOptions();
-        retryOptions.maxRetryWaitTimeInSeconds(60);
-        policy.retryOptions(retryOptions);
-        return CosmosDBConfig.builder(uri, key, dbName).requestOptions(options).connectionPolicy(policy).build();
-    }
-    */
-    
+   
 }
--- src/main/java/com/microsoft/cse/helium/app/services/configuration/IConfigurationService.java
+++ src/main/java/com/microsoft/cse/helium/app/services/configuration/IConfigurationService.java
@@ -5,11 +5,7 @@ import java.util.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 
-//import com.microsoft.azure.spring.data.cosmosdb.config.AbstractCosmosConfiguration;
-//import com.microsoft.azure.spring.data.cosmosdb.config.CosmosDBConfig;
-
 public interface IConfigurationService {
 
-    //public CosmosDBConfig getConfig();
     public Dictionary<String, String> getConfigEntries();
 }
