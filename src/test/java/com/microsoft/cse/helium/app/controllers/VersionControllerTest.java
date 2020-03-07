592f136d3e486b9c026a03449f7384260cc6e06a
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-03-07 00:17:24 +0000

    minor cleanup

--- src/test/java/com/microsoft/cse/helium/app/controllers/VersionControllerTest.java
+++ src/test/java/com/microsoft/cse/helium/app/controllers/VersionControllerTest.java
@@ -1,18 +1,13 @@
 package com.microsoft.cse.helium.app.controllers;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import static org.junit.Assert.*;
 
-import com.microsoft.cse.helium.app.config.BuildConfig;
-import com.microsoft.cse.helium.app.models.HeliumConfig;
 
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import com.microsoft.cse.helium.app.models.HeliumConfig;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.slf4j.Logger;
@@ -21,24 +16,10 @@ import org.slf4j.LoggerFactory;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=HeliumConfig.class)
-//@org.springframework.beans.factory.annotation.Autowired(required=true)
-
-//@SpringApplicationConfiguration(classes={VersionController.class, BuildConfig.class})
-//@AutoConfigureWebTestClient
 public class VersionControllerTest{
 
     @Mock
     private VersionController versionController;
-
-    // @InjectMocks
-    // private BuildConfig buildConfig;
-
-    //@InjectMocks
-    //private BuildProperties buildProperties;
-
-    // @Autowired
-    // private WebTestClient webClient;
-
 
     private static final Logger _logger = LoggerFactory.getLogger(VersionControllerTest.class);
 
@@ -49,11 +30,7 @@ public class VersionControllerTest{
         .configureClient()
         .baseUrl("/")
         .build();
-        //BuildConfig buildConfig = mock (BuildConfig.class);
-        //buildConfig.getBuildVersion();
-        // assertTrue(buildConfig != null);
-        // _logger.error("Hello Sanjeev " + buildConfig.getBuildVersion());
-        //webClient = new WebTestClient();
+
         assertTrue(webClient != null);
         webClient
         .get()
@@ -61,7 +38,7 @@ public class VersionControllerTest{
         .exchange()
         .expectStatus()
         .isOk();
-        _logger.error("hola hola sanjeev");
+        _logger.error("Passed the Unit tests, got a 200 OK!");
     }
 
 
