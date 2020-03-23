9cd203208f297a29c5e43324a21ca6b7cbbb615e
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-03-23 11:51:38 -0700

    updaing the app package (#133)

--- src/main/java/com/microsoft/cse/helium/app/HeliumJavaApplication.java
+++ src/main/java/com/microsoft/cse/helium/app/HeliumJavaApplication.java
@@ -1,4 +1,4 @@
-package com.microsoft.cse.helium;
+package com.microsoft.cse.helium.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
