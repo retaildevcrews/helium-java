3b4374a3802f557c07aa439ad37b3aeae1e76bef
Jyotsna Ravikumar <jyraviku@microsoft.com>
2020-03-25 14:42:29 -0700

    movies query, pgsize, pagenumber impl

--- src/main/java/com/microsoft/cse/helium/app/controllers/MoviesController.java
+++ src/main/java/com/microsoft/cse/helium/app/controllers/MoviesController.java
@@ -1,5 +1,6 @@
 package com.microsoft.cse.helium.app.controllers;
 
+import com.microsoft.cse.helium.app.Constants;
 import com.microsoft.cse.helium.app.dao.MoviesDao;
 import com.microsoft.cse.helium.app.models.Movie;
 import com.microsoft.cse.helium.app.utils.ParameterValidator;
@@ -8,14 +9,19 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -66,5 +72,74 @@ public class MoviesController {
     }
   }
 
+  /**
+   * getAllMovies.
+   */
+  @RequestMapping(
+      value = "",
+      method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Get all movies", notes = "Retrieve and return all movies")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "List of movie objects")})
+  public Object getAllMovies(
+      @ApiParam(value = "(query) (optional) The term used to search Movie name")
+      @RequestParam("q") final Optional<String> query,
+      @ApiParam(value = "1 based page index", defaultValue = "1") @RequestParam
+          Optional<String> pageNumber,
+      @ApiParam(value = "page size (1000 max)", defaultValue = "100") @RequestParam
+          Optional<String> pageSize) {
+
+    String q = null;
+
+    if (query.isPresent() && !StringUtils.isEmpty(query.get())
+        && query.get() != null && !query.get().isEmpty()) {
+      if (validator.isValidSearchQuery(query.get())) {
+        q = query.get().trim().toLowerCase().replace("'", "''");
+      } else {
+        logger.error("Invalid q(search) parameter");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(
+            "Invalid q(search) parameter", headers, HttpStatus.BAD_REQUEST);
+      }
+    }
+
+    Integer pageNo = 0;
+    if (pageNumber.isPresent() && !StringUtils.isEmpty(pageNumber.get())) {
+      if (!validator.isValidPageNumber(pageNumber.get())) {
+        logger.error("Invalid pageNumber parameter");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(
+            "Invalid pageNumber parameter", headers, HttpStatus.BAD_REQUEST);
+      } else {
+        pageNo = Integer.parseInt(pageNumber.get());
+      }
+    }
+
+    Integer pageSz = Constants.DEFAULT_PAGE_SIZE;
+    if (pageSize.isPresent() && !StringUtils.isEmpty(pageSize.get())) {
+      if (!validator.isValidPageSize(pageSize.get())) {
+        logger.error("Invalid pageSize parameter");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>("Invalid pageSize parameter", headers,
+            HttpStatus.BAD_REQUEST);
+      } else {
+        pageSz = Integer.parseInt(pageSize.get());
+      }
+    }
+
+    try {
+      pageNo--;
+      if (pageNo < 0) {
+        pageNo = 0;
+      }
+      return moviesDao.getMovies(q, pageNo * pageSz, pageSz);
+    } catch (Exception ex) {
+      return new ResponseEntity<>("MoviesControllerException",
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
 }
--- src/main/java/com/microsoft/cse/helium/app/dao/MoviesDao.java
+++ src/main/java/com/microsoft/cse/helium/app/dao/MoviesDao.java
@@ -3,25 +3,37 @@ package com.microsoft.cse.helium.app.dao;
 import static com.microsoft.azure.spring.data.cosmosdb.exception.CosmosDBExceptionUtils.findAPIExceptionHandler;
 
 import com.azure.data.cosmos.CosmosClient;
+import com.azure.data.cosmos.CosmosItemProperties;
+import com.azure.data.cosmos.FeedResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.spring.data.cosmosdb.core.convert.ObjectMapperFactory;
+import com.microsoft.cse.helium.app.models.Actor;
 import com.microsoft.cse.helium.app.models.Movie;
 import com.microsoft.cse.helium.app.services.configuration.IConfigurationService;
 import com.microsoft.cse.helium.app.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class MoviesDao extends BaseCosmosDbDao {
+  private static final Logger logger = LoggerFactory.getLogger(MoviesDao.class);
+
 
   @Autowired CommonUtils utils;
-  /*
+
   private static String movieSelect =
       "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime,"
           + "m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  ";
 
   private static String movieContains = "and contains(m.textSearch, \"%s\") ";
   private static String movieOrderBy = " order by m.textSearch ";
-  private static String movieOffset = " offset %d limit %d ";*/
+  private static String movieOffset = " offset %d limit %d ";
 
   /** MoviesDao. */
   public MoviesDao(IConfigurationService configService) {
@@ -44,5 +56,48 @@ public class MoviesDao extends BaseCosmosDbDao {
                 })
             .onErrorResume(throwable -> findAPIExceptionHandler("Failed to find item", throwable));
     return movie;
+  }
+
+  /** getAMovies. */
+  public Flux<Actor> getMovies(String query, Integer pageNumber, Integer pageSize) {
+    ObjectMapper objMapper = ObjectMapperFactory.getObjectMapper();
+
+    String contains = "";
+
+    if (query != null) {
+      contains = String.format(movieContains, query);
+    }
+
+    String actorQuery =
+        movieSelect + contains + movieOrderBy + String.format(movieOffset, pageNumber, pageSize);
+
+    logger.info("movieQuery " + actorQuery);
+    Flux<FeedResponse<CosmosItemProperties>> feedResponse =
+        this.context
+            .getBean(CosmosClient.class)
+            .getDatabase(this.cosmosDatabase)
+            .getContainer(this.cosmosContainer)
+            .queryItems(actorQuery, this.feedOptions);
+
+    Flux<Actor> selectedActors =
+        feedResponse
+            .flatMap(
+                flatFeedResponse -> {
+                  return Flux.fromIterable(flatFeedResponse.results());
+                })
+            .flatMap(
+                cosmosItemProperties -> {
+                  try {
+                    return Flux.just(
+                        objMapper.readValue(cosmosItemProperties.toJson(), Actor.class));
+                  } catch (JsonMappingException e) {
+                    e.printStackTrace();
+                  } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                  }
+                  return Flux.empty();
+                });
+
+    return selectedActors;
   }
 }
