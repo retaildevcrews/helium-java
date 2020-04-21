61069adfcb6730ba7bb897602c4185ab9d7585d7
Sanjeev Dwivedi <sanjeevd@microsoft.com>
2020-04-21 23:42:44 +0000

    removing unused entry from POM.xml and removing unnecessary webflux file

--- src/main/java/com/microsoft/cse/helium/app/config/WebfluxConfig.java
+++ src/main/java/com/microsoft/cse/helium/app/config/WebfluxConfig.java
@@ -9,13 +9,6 @@ public class WebfluxConfig implements WebFluxConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-    // registry.addResourceHandler("/swagger-ui.html**")
-    //     .addResourceLocations("classpath:/META-INF/resources/");
-    // registry.addResourceHandler("/webjars/**")
-    //     .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
-
     registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
     registry.addResourceHandler("/swagger-ui.html**")
