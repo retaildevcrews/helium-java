f9167e95e5e795c84022c25f8043b8f7577c9610
jofultz <jofultz@microsoft.com>
2020-04-21 16:01:04 -0500

    healthz code re-merge (#201)
    * basic healthz version
    
    * healthz api impl
    
    * partially working sample
    
    * partially working sample
    
    * removed some comments
    
    * removed doOnSuccess
    
    * refactor to build result dict
    
    * comments
    
    * partially working with flux
    
    * removed some commented code
    
    * updated to handle flux in chain and adjust time calculation
    
    * all calls made; converted list->dict
    
    * refactored status and call code for reuse in healthz
    
    * added healthz, removed dead code
    
    * removed comments, changed to enum
    
    * udpate generics to use <String, Object> so Long serialize properly
    
    Co-authored-by: Jyotsna Ravikumar <jyraviku@microsoft.com>

--- src/main/java/com/microsoft/cse/helium/app/Constants.java
+++ src/main/java/com/microsoft/cse/helium/app/Constants.java
@@ -26,4 +26,7 @@ public final class Constants {
   public static final String ACTOR_CONTROLLER_EXCEPTION = "ActorControllerException";
   public static final String MOVIE_CONTROLLER_EXCEPTION = "MovieControllerException";
 
+  public static final String webInstanceRole = "WEBSITE_ROLE_INSTANCE_ID";
+
+
 }
