115787466868d3ce98acdcf1407a4b297b504c35
Joseph Fultz <jofultz@microsoft.com>
2020-05-27 04:39:36 +0000

    added generic type for ResponseEntity<>

--- src/main/java/com/cse/helium/app/controllers/IndexController.java
+++ src/main/java/com/cse/helium/app/controllers/IndexController.java
@@ -20,13 +20,13 @@ public class IndexController {
   /** indexController. redirect index.html to swagger so website always serves something. */
   @GetMapping("/")
   @SuppressFBWarnings({"DMI_HARDCODED_ABSOLUTE_FILENAME", "OS_OPEN_STREAM"})
-  public ResponseEntity indexController(ServerHttpResponse response) throws IOException {
+  public ResponseEntity<?> indexController(ServerHttpResponse response) throws IOException {
     response.setStatusCode(HttpStatus.OK);
     Resource resource = new ClassPathResource("static/swagger-ui.html");
     InputStreamResource inputStreamResource = new InputStreamResource(resource.getInputStream());
     HttpHeaders httpHeaders = new HttpHeaders();
     httpHeaders.setContentLength(resource.getInputStream().available());
     httpHeaders.setContentType(MediaType.TEXT_HTML);
-    return new ResponseEntity(inputStreamResource, httpHeaders, HttpStatus.OK);
+    return new ResponseEntity<>(inputStreamResource, httpHeaders, HttpStatus.OK);
   }
 }
