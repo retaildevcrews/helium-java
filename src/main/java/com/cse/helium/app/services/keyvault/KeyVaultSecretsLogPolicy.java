eeb99fdb5ac73d3ffb9fd3a0846928afb4ae25f2
Joseph Fultz <jofultz@microsoft.com>
2020-06-04 22:54:56 +0000

    wip cleaning logging for kv

--- src/main/java/com/cse/helium/app/services/keyvault/KeyVaultSecretsLogPolicy.java
+++ src/main/java/com/cse/helium/app/services/keyvault/KeyVaultSecretsLogPolicy.java
@@ -75,7 +75,8 @@ public class KeyVaultSecretsLogPolicy implements HttpPipelinePolicy {
   @Override
   public Mono<HttpResponse> process(HttpPipelineCallContext context, HttpPipelineNextPolicy next) {
     // No logging will be performed, trigger a no-op.
-    if (httpLogDetailLevel == HttpLogDetailLevel.NONE) {
+    if (httpLogDetailLevel == HttpLogDetailLevel.NONE ||
+      httpLogDetailLevel == HttpLogDetailLevel.BASIC) {
       return next.process();
     }
 
