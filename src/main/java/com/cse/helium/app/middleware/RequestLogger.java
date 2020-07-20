c54c2908567b0aa7400bd194e8851ddd3317104a
Siva Mullapudi <sivamu@microsoft.com>
2020-07-20 22:22:45 +0000

    Request logging Middleware
    filling in more details
    
    adjusting comment
    
    Add newline
    
    Fine tune method comment

---
+++ src/main/java/com/cse/helium/app/middleware/RequestLogger.java
@@ -0,0 +1,63 @@
+package com.cse.helium.app.middleware;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RequestLogger implements WebFilter {
+
+  private static final Logger logger =   LogManager.getLogger();
+ 
+  /**
+   * filter gathers the request and response metadata and logs
+   *   the results to console.
+  */
+  @Override
+  public Mono<Void> filter (ServerWebExchange serverWebExchange, 
+    WebFilterChain webFilterChain) {
+      // get request metadata
+      String remoteAddress = serverWebExchange.getRequest().getRemoteAddress().getHostString();
+      String pathQueryString = getPathQueryString(serverWebExchange.getRequest());
+
+      // set start time
+      long startTime = System.currentTimeMillis();
+      
+      // process next handler
+      return webFilterChain.filter(serverWebExchange).doFinally(signalType -> {
+        // compute request duration and get status code
+        long duration = System.currentTimeMillis() - startTime;
+        int statusCode = serverWebExchange.getResponse().getStatusCode().value();
+        
+        // don't log favicon.ico 404s
+        if (pathQueryString.startsWith("/favicon.ico")) {
+          return;
+        }
+
+        // don't log if log level >= warn but response code < 400
+        if (logger.getLevel().isMoreSpecificThan(Level.WARN) && statusCode < 400) {
+          return;
+        }
+
+        // log results to console
+        logger.info(String.format("%s\t%s\t%s\t%s", statusCode, duration, remoteAddress, pathQueryString));
+      });
+  }
+
+  /** getPathQueryString returns the path and query string if it exists */
+  private String getPathQueryString (ServerHttpRequest request) {
+    String pathQueryString = request.getURI().getPath();
+    String query = request.getURI().getQuery();
+    if (query != null) {
+      pathQueryString += "?" + query;
+    }
+
+    return pathQueryString;
+  }
+}
