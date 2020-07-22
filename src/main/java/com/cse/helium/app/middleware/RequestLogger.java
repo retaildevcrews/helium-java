0fa50ab3aacdac48d9e9f0c9c227a88ee338b2af
sivamu <67930841+sivamu@users.noreply.github.com>
2020-07-22 09:14:05 -0500

    Request logging Middleware (#280)
    * Request logging Middleware
    
    filling in more details
    
    adjusting comment
    
    Add newline
    
    Fine tune method comment
    
    * Syntax fixes
    
    * Addressing build errors
    
    * Break up long line
    
    * Address review comments
    
    * Updating version

---
+++ src/main/java/com/cse/helium/app/middleware/RequestLogger.java
@@ -0,0 +1,76 @@
+package com.cse.helium.app.middleware;
+
+import java.net.InetSocketAddress;
+import java.text.MessageFormat;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RequestLogger implements WebFilter {
+
+  private static final Logger logger =   LogManager.getLogger(RequestLogger.class);
+ 
+  /**
+   * filter gathers the request and response metadata and logs
+   *   the results to console.
+  */
+  @Override
+  public Mono<Void> filter(ServerWebExchange serverWebExchange, 
+      WebFilterChain webFilterChain) {
+    // get request metadata
+    String requestAddress = getRequestAddress(
+        serverWebExchange.getRequest().getRemoteAddress());
+    String pathQueryString = getPathQueryString(serverWebExchange.getRequest());
+
+    // set start time
+    long startTime = System.currentTimeMillis();
+      
+    // process next handler
+    return webFilterChain.filter(serverWebExchange).doFinally(signalType -> {
+      // compute request duration and get status code
+      long duration = System.currentTimeMillis() - startTime;
+      int statusCode = serverWebExchange.getResponse().getStatusCode().value();
+      
+      // don't log favicon.ico 404s
+      if (pathQueryString.startsWith("/favicon.ico")) {
+        return;
+      }
+
+      // don't log if log level >= warn but response code < 400
+      if (logger.getLevel().isMoreSpecificThan(Level.WARN) && statusCode < 400) {
+        return;
+      }
+
+      // log results to console
+      logger.info(MessageFormat.format(
+          "{0}\t{1}\t{2}\t{3}", 
+          statusCode, duration, requestAddress, pathQueryString));
+    });
+  }
+
+  /** getRequestAddress returns the request IP address if it exists. */
+  private String getRequestAddress(InetSocketAddress requestAddress) {
+    if (requestAddress != null) {
+      return requestAddress.getHostString();
+    }
+    return "";
+  }
+
+  /** getPathQueryString returns the path and query string if it exists. */
+  private String getPathQueryString(ServerHttpRequest request) {
+    String pathQueryString = request.getURI().getPath();
+    String query = request.getURI().getQuery();
+    if (query != null) {
+      pathQueryString += "?" + query;
+    }
+
+    return pathQueryString;
+  }
+}
