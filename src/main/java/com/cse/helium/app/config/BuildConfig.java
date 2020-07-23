d3ba828efc01b15d0028e0193eb9f0e8ca04fcf1
Siva Mullapudi <sivamu@microsoft.com>
2020-07-23 16:07:35 +0000

    Addressing comments

--- src/main/java/com/cse/helium/app/config/BuildConfig.java
+++ src/main/java/com/cse/helium/app/config/BuildConfig.java
@@ -1,6 +1,9 @@
 package com.cse.helium.app.config;
 
 import java.text.MessageFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +24,22 @@ public class BuildConfig {
    * getBuildVersion. Returns build version.
    */
   public String getBuildVersion() {
+    // An Instant represents a moment on the timeline in UTC with a resolution of up to nanoseconds.
+    Instant buildTime = context.getBean(BuildProperties.class).getTime();
+
+    // major.minor.MMd.hhmm
     String major = context.getBean(BuildProperties.class).getVersion();
 
+    DateTimeFormatter formatter = DateTimeFormatter
+        .ofPattern("MMdd.hhmm")
+        .withZone(ZoneId.of("UTC"));
+
+    String formattedDateTime = formatter.format(buildTime);
+
     if (logger.isInfoEnabled()) {
-      logger.info(MessageFormat.format("version {0}", major));
+      logger.info(MessageFormat.format("version {0}.{1}", major, formattedDateTime));
     }
-    return major;
+    return major + "+" + formattedDateTime;
 
   }
 }
--- src/main/java/com/cse/helium/app/controllers/VersionController.java
+++ src/main/java/com/cse/helium/app/controllers/VersionController.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 
@@ -38,20 +39,20 @@ public class VersionController {
       value = "/version",
       produces = MediaType.APPLICATION_JSON_VALUE)
   public Mono<Map<String, String>> version(ServerHttpResponse response) {
-    String apiVersion = "1.0";
     try {
-      apiVersion = swaggerConfig.getInfo().get("version");      
+      // build the json result body
+      LinkedHashMap<String, String> versionResult = new LinkedHashMap<>();
+
+      versionResult.put("appVersion", 
+          context.getBean(BuildConfig.class).getBuildVersion());
+      versionResult.put("apiVersion", swaggerConfig.getInfo().get("version"));
+
+      response.setStatusCode(HttpStatus.OK);
+      return Mono.just(versionResult);
     } catch (Exception ex) {
       logger.error("Error received in VersionController.", ex);
+      return Mono.error(new ResponseStatusException(
+        HttpStatus.INTERNAL_SERVER_ERROR, "version Error"));
     }
-
-    // build the json result body
-    LinkedHashMap<String, String> versionResult = new LinkedHashMap<>();
-
-    versionResult.put("appVersion", context.getBean(BuildConfig.class).getBuildVersion());
-    versionResult.put("apiVersion", apiVersion);
-
-    response.setStatusCode(HttpStatus.OK);
-    return Mono.just(versionResult);
   }
 }
