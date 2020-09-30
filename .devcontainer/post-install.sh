c7814360a3c968a4c7a8a05a197e66277a84cf39
sivamu <67930841+sivamu@users.noreply.github.com>
2020-09-30 21:56:49 +0000

    Java docker environment for codespaces
    Update base image
    
    Proper variant
    
    Dockerfile customize
    
    Mount docker sock
    
    Install maven
    
    Trying out post-install script
    
    revert to bash
    
    Final changes
    
    Revert copy statements
    
    Trying out diff context
    
    Trying out diff context
    
    Test with build and image
    
    image by default
    
    webv installed in post-install script
    
    Simplify dockerfile
    
    Dockerfile cleanup + add ref to docker-debian script
    
    Clarify docker sock usage
    
    Few more edits
    
    Add option for local dockerfile

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-# docker bash-completion
-sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
-
-DEBIAN_FRONTEND=dialog
-
 # copy vscode files
 mkdir -p .vscode && cp docs/vscode-template/* .vscode
 
-# source the bashrc-append from the repo
-# you can add project specific settings to .bashrc-append and 
-# they will be added for every user that clones the repo with Codespaces
-# including keys or secrets could be a SECURITY RISK
-echo "" >> ~/.bashrc
-echo ". ${PWD}/.devcontainer/.bashrc-append" >> ~/.bashrc
-
 # set auth type
+export PATH="$PATH:~/.dotnet/tools"
 export AUTH_TYPE=CLI
+
+# install WebV global tool
+dotnet tool install -g webvalidate
+
+# update .bashrc
+echo "" >> ~/.bashrc
+echo 'export PATH="$PATH:~/.dotnet/tools"' >> ~/.bashrc
+echo "export AUTH_TYPE=CLI" >> ~/.bashrc
+
